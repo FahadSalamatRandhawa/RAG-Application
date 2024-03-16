c8cf3148cff7bc04c7b16ff73fce6fc54c3cf8f0
fahadsalamatrandhawa <dreamtwister61814@gmail.com>
2024-03-16 19:32:33 +0500

    change button

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         <Input
           type="file" multiple
         onChange={(e)=>setFiles(e.target.files)} />
-        <Button disabled={!files} onClick={handle_File_Upload} >Upload to db</Button>
+        <button disabled={!files} onClick={handle_File_Upload} >Upload to db</button>
       </div>
 
       {/* Chat section */}
